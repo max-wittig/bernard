f6ba2fe95e1f76d3b20709dfafebb8eb30f717e4
Max Wittig <max.wittig95@gmail.com>
2018-09-16 22:38:07 +0200

    feat: use command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,36 @@
-extern crate ipnetwork;
-extern crate pnet;
+#[macro_use]
+extern crate log;
+extern crate prometheus;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_yaml;
+extern crate stderrlog;
+#[macro_use]
+extern crate structopt;
 extern crate tempfile;
 extern crate xml;
-#[macro_use]
-extern crate log;
-#[macro_use]
-extern crate prometheus;
-use std::io::prelude::*;
 
-use prometheus::{Opts, Registry, Gauge, GaugeVec, TextEncoder, Encoder};
+use prometheus::{Encoder, GaugeVec, Opts, Registry, TextEncoder};
 use scanner::Scanner;
 use std::collections::HashMap;
 use std::fmt;
+use std::fmt::Display;
 use std::fs::File;
-use std::io;
+use std::io::prelude::*;
 use std::io::Read;
+use structopt::StructOpt;
 
 mod scanner;
-
 
 #[derive(Debug, Serialize, Deserialize)]
 enum ConfigError {
     ReadError,
+}
+
+impl Display for ConfigError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self)
+    }
 }
 
 impl From<std::io::Error> for ConfigError {
@@ -51,18 +57,44 @@ fn get_config(filename: &str) -> Result<Config, ConfigError> {
     Ok(serde_yaml::from_str(&file_content)?)
 }
 
-fn main() {
-    let config = match get_config("config.yaml") {
-        Ok(x) => x,
-        Err(_) => panic!("Could not read config!"),
-    };
+/// Network-scanner
+#[derive(StructOpt, Debug)]
+#[structopt()]
+struct Opt {
+    #[structopt(short = "q", long = "quiet")]
+    quiet: bool,
+    /// Verbose mode (-v, -vv, -vvv, etc)
+    #[structopt(short = "v", long = "verbose", parse(from_occurrences))]
+    verbose: usize,
+    /// Timestamp (sec, ms, ns, none)
+    #[structopt(short = "t", long = "timestamp")]
+    ts: Option<stderrlog::Timestamp>,
+    /// Path to config file
+    #[structopt(short = "c", long = "config")]
+    config: String,
+    /// IP in CIDR notation of the network you want to scan, e.g. 192.168.178.28/24
+    #[structopt(short = "n", long = "network")]
+    network: String,
+}
 
+fn main() {
+    let opt = Opt::from_args();
+    stderrlog::new()
+        .module(module_path!())
+        .quiet(opt.quiet)
+        .verbosity(opt.verbose)
+        .timestamp(opt.ts.unwrap_or(stderrlog::Timestamp::Off))
+        .init()
+        .unwrap();
+
+    let config = get_config(&opt.config).expect("Could not read config file!");
     info!("Config loaded");
-    let scanner = Scanner::new();
+    let scanner = Scanner::new(&opt.network);
+    info!("Running nmap to detect devices...");
     let people_online = scanner.get_people_online(&config.people);
-    println!("Online are: {}", people_online.join(", "));
+    info!("Online: {}", people_online.join(", "));
     //let people_online = vec!("max".to_string(), "nadia".to_string());
-    let mut people_status_map : HashMap<String, f64> = HashMap::new();
+    let mut people_status_map: HashMap<String, f64> = HashMap::new();
     for person in config.people {
         if people_online.contains(&person.0) {
             people_status_map.insert(person.0.clone(), 1.0);
@@ -70,17 +102,19 @@ fn main() {
             people_status_map.insert(person.0.clone(), 0.0);
         }
     }
-    generate_node_export(&people_status_map);
+    write_metrics_file(&people_status_map);
 }
 
-fn generate_node_export(people: &HashMap<String, f64>) {
+fn write_metrics_file(people: &HashMap<String, f64>) {
     let r = Registry::new();
-    let mut gauge_vec_opts = Opts::new("people", "People online status");
+    let gauge_vec_opts = Opts::new("people", "People online status");
 
     let gauge_vec: GaugeVec = GaugeVec::new(gauge_vec_opts, &["name"]).unwrap();
     r.register(Box::new(gauge_vec.clone())).unwrap();
     for person in people {
-        gauge_vec.with_label_values(&[person.0.as_str()]).set(*person.1);
+        gauge_vec
+            .with_label_values(&[person.0.as_str()])
+            .set(*person.1);
     }
 
     // Gather the metrics.
@@ -88,9 +122,9 @@ fn generate_node_export(people: &HashMap<String, f64>) {
     let encoder = TextEncoder::new();
     let metric_families = r.gather();
     encoder.encode(&metric_families, &mut buffer).unwrap();
-
-    // Output to the standard output.
-    println!("{}", String::from_utf8(buffer.clone()).unwrap());
+    info!("Exporter file: {}", String::from_utf8(buffer.clone()).unwrap());
     let mut out_file = File::create("metrics.txt").unwrap();
-    out_file.write_all(String::from_utf8(buffer).unwrap().as_bytes()).unwrap();
+    out_file
+        .write_all(String::from_utf8(buffer).unwrap().as_bytes())
+        .expect("Could not write output file!");
 }
--- src/scanner.rs
+++ src/scanner.rs
@@ -1,16 +1,12 @@
 use HashMap;
-use ipnetwork::Ipv4Network;
-use pnet::datalink;
 use std::io::BufReader;
-use std::io::Read;
-use std::net::IpAddr;
 use std::process::Command;
-use std::vec;
-use std::fmt;
 use tempfile::NamedTempFile;
 use xml::reader::{EventReader, XmlEvent};
 
-pub struct Scanner {}
+pub struct Scanner {
+    cidr: String
+}
 
 #[derive(Debug, Clone)]
 struct Device {
@@ -42,28 +38,31 @@ impl Device {
 }
 
 impl Scanner {
-    fn get_devices(&self, cidr: &str) -> Vec<Device> {
+    fn get_devices(&self) -> Vec<Device> {
         let file = NamedTempFile::new().unwrap();
-        let path = file.path().to_str().clone().unwrap().to_string();
+        let path = file.path().to_str().unwrap().to_string();
         Command::new("nmap")
-            .arg("-sn")
-            .arg("192.168.178.28/24")
+            .arg("-sF")
+            .arg(self.cidr.as_str())
             .arg("-oX")
             .arg(path)
-        .output().expect("Error, while running nmap");
+            .output()
+            .expect("Error, while running nmap");
 
         let buf_reader = BufReader::new(file.as_file());
         let xml_reader = EventReader::new(buf_reader);
 
-        let mut hosts: Vec<Device> = vec!();
+        let mut hosts: Vec<Device> = vec![];
         let mut current_host: Option<Device> = None;
         let mut is_online: bool = false;
         for entry in xml_reader {
             match entry {
-                Ok(XmlEvent::StartElement { name, attributes, .. }) => {
+                Ok(XmlEvent::StartElement {
+                    name, attributes, ..
+                }) => {
                     if name.local_name.eq("host") {
-                        //println!("Start{:?}", attributes);
                         current_host = Some(Device::new());
+                        debug!("Found new host");
                     }
                     let host = match &mut current_host {
                         Some(x) => x,
@@ -71,10 +70,9 @@ impl Scanner {
                     };
                     if name.local_name.eq("status") {
                         for attr in &attributes {
-                            if attr.name.local_name.eq("state") {
-                                if attr.value.eq("up") {
-                                    is_online = true;
-                                }
+                            if attr.name.local_name.eq("state") && attr.value.eq("up") {
+                                is_online = true;
+                                debug!("Host is online")
                             }
                         }
                     }
@@ -89,6 +87,7 @@ impl Scanner {
                                 addr_type = Some(attr.value.clone());
                             }
                         }
+                        debug!("Found address for host");
 
                         match addr_type {
                             Some(ref x) if x.eq("mac") => host.set_mac(addr.unwrap()),
@@ -103,7 +102,7 @@ impl Scanner {
                             }
                         }
                     }
-                    continue
+                    continue;
                 }
                 Ok(XmlEvent::EndElement { name }) => {
                     if name.local_name.eq("host") {
@@ -112,56 +111,35 @@ impl Scanner {
                         }
                         is_online = false;
                     }
-                    continue
+                    continue;
                 }
                 Err(e) => {
-                    println!("Error: {}", e);
-                    break;
+                    panic!("Error, while parsing xml: {}", e);
                 }
                 _ => {}
-
             }
         }
         hosts
     }
 
-    pub fn new() -> Scanner {
-        Scanner{}
-    }
-
-    fn get_interfaces(&self) -> Vec<Ipv4Network> {
-        let mut interfaces: Vec<Ipv4Network> = vec!();
-        for interface in datalink::interfaces() {
-            //println!("Available interfaces: {:?}", interface);
-            let ipv4_address = match interface.ips[0].ip() {
-                IpAddr::V4(ip) => ip,
-                _ => continue,
-            };
-            let network = match Ipv4Network::new(ipv4_address, interface.ips[0].prefix()) {
-                Ok(x) => x,
-                Err(_) => continue,
-            };
-            interfaces.push(network);
+    pub fn new(cidr: &str) -> Scanner {
+        Scanner {
+            cidr: cidr.to_string()
         }
-        interfaces
     }
 
     pub fn get_people_online(&self, people_map: &HashMap<String, Vec<String>>) -> Vec<String> {
-        let mut people_online: Vec<String> = vec!();
-        let interfaces: Vec<Ipv4Network> = self.get_interfaces();
-        for iface in interfaces {
-            info!("Scanning over interface {:?}", iface);
-            let online_devices = self.get_devices(format!("{}/{}", iface.network(), iface.prefix()).as_str());
-            let online_macs: Vec<String> = online_devices.iter().map(|x| x.mac.clone()).collect();
-            for person in people_map {
-                for mac in person.1 {
-                    let mac = mac.to_ascii_uppercase();
-                    if online_macs.contains(&mac) {
-                        if !people_online.contains(person.0) {
-                            people_online.push(person.0.clone());
-                        }
-                        break;
+        let mut people_online: Vec<String> = vec![];
+        let online_devices = self.get_devices();
+        let online_macs: Vec<String> = online_devices.iter().map(|x| x.mac.clone()).collect();
+        for person in people_map {
+            for mac in person.1 {
+                let mac = mac.to_ascii_uppercase();
+                if online_macs.contains(&mac) {
+                    if !people_online.contains(person.0) {
+                        people_online.push(person.0.clone());
                     }
+                    break;
                 }
             }
         }
